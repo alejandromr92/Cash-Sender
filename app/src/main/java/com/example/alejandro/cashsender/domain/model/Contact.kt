d5108ea62b9a8f682bc79a47baed30d359ac6367
Alejandro Muñoz <alejandro_mr92@hotmail.com>
2019-01-10 22:43:47 +0100

    Minor fixes.

--- app/src/main/java/com/example/alejandro/cashsender/domain/model/Contact.kt
+++ app/src/main/java/com/example/alejandro/cashsender/domain/model/Contact.kt
@@ -24,6 +24,26 @@ class Contact(
         return 0
     }
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Contact
+
+        if (name != other.name) return false
+        if (thumbnail != other.thumbnail) return false
+        if (phoneNumber != other.phoneNumber) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = name.hashCode()
+        result = 31 * result + thumbnail.hashCode()
+        result = 31 * result + phoneNumber.hashCode()
+        return result
+    }
+
     companion object CREATOR : Parcelable.Creator<Contact> {
         override fun createFromParcel(parcel: Parcel): Contact {
             return Contact(parcel)
@@ -33,4 +53,6 @@ class Contact(
             return arrayOfNulls(size)
         }
     }
+
+
 }
